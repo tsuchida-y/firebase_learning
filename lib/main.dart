50ec5b05678c4cab34766b35cbf2f21232d6eece
tsuchida <yu41926t.tutida@icloud.com>
2024-12-20 02:22:45 +0900

    データ取得成功

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  print("初期化前");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  print("初期化後");
   runApp(const MyApp());
 }
 
@@ -40,37 +38,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
 //firebaseからデータを取得する
-  //   void _fetchFirebaseData() async{
-  //     print("await前");
-  //   await FirebaseFirestore.instance.collection("posts")
-  //   .get()//postsコレクション内の全てのドキュメントを取得
-  //   .then((event) {//データ取得完了後に実行されるコールバック関数を定義
-  //   print("await後");
-  //     for(var doc in event.docs) {
-  //       print("${doc.id}=>${doc.data()}");
-  //     }
-  //   });
-  //   print("メソッド終了");
-  // }
-void _fetchFirebaseData() async {
-  try {
-    QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection("posts").get();
-    for (var doc in querySnapshot.docs) {
-      print("${doc.id} => ${doc.data()}");
-    }
-  } catch (e) {
-    print("Error fetching data: $e");
+    void _fetchFirebaseData() async{
+    await FirebaseFirestore.instance.collection("posts").get().then((event) {
+      for(var doc in event.docs) {
+        print("${doc.id}=>${doc.data()}");
+      }
+    });
   }
-}
+// void _fetchFirebaseData() async {
+//   try {
+//     QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection("posts").get();
+//     for (var doc in querySnapshot.docs) {
+//       print("${doc.id} => ${doc.data()}");
+//     }
+//   } catch (e) {
+//     print("Error fetching data: $e");
+//   }
+// }
 
 
   @override
@@ -83,17 +69,13 @@ void _fetchFirebaseData() async {
 
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
 
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'You have pushed the button this many times:',
             ),
           ],
         ),
