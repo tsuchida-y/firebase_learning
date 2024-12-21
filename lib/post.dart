e13ac26e1ea8b81025812a7c3f2273d0bf39d5db
tsuchida <yu41926t.tutida@icloud.com>
2024-12-22 02:23:52 +0900

    postを管理するクラスの作成

---
+++ lib/post.dart
@@ -0,0 +1,13 @@
+class Post{
+  String id;
+  String text;
+  DateTime createdAt;
+  DateTime updatedAt;
+
+  Post({
+    required this.id,
+    required this.text,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+}
